fb714a5ed32240af2cf07c40c19498be0a3cc2d5
r4wm <raymondmintz11@gmail.com>
2018-10-20 22:48:01 -0700

    cleanup module

--- kjv.go
+++ kjv.go
@@ -28,16 +28,3 @@ func GetChapter(book string, chapter int, verses []KJVVerse) *KJVBook {
 				Verses:  verses}},
 	}
 }
-
-// func main() {
-// 	var verses []KJVVerse
-
-// 	verse := KJVVerse{1, "In the beginning God created the heaven and the earth."}
-// 	verses = append(verses, verse)
-// 	verse2 := KJVVerse{2, "And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters."}
-// 	verses = append(verses, verse2)
-
-// 	query := GetChapter("Genesis", 1, verses)
-// 	jsonQuery, _ := json.MarshalIndent(query, "", "  ")
-// 	fmt.Printf("%s\n", jsonQuery)
-// }
