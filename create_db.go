ac5808a07c13c83d89a8aa9c835540e19493f790
r4wm <raymondmintz11@gmail.com>
2018-11-18 20:11:13 -0800

    First working kjv database self generating

--- create_db.go
+++ create_db.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 
@@ -45,6 +45,13 @@ func IsNumberedBook(firstPart string) bool {
 }
 
 func pullKJVText() {
+	fmt.Println("Starting sqlite3 db creation. ")
+	database, _ := sql.Open("sqlite3", "data/kjv.sqlite3.db")
+	defer database.Close()
+
+	statement, _ := database.Prepare("create table if not exists kjv(book string not null, chapter int, verse int, text string)")
+	statement.Exec()
+
 	url := "https://raw.githubusercontent.com/R4wm/bible/master/data/bible.txt"
 
 	resp, err := http.Get(url)
@@ -64,22 +71,26 @@ func pullKJVText() {
 			//This is Song of Solomon book, special case where book name has multiple words
 			verse.Book = fmt.Sprintf("%s %s %s", brokenString[0], brokenString[1], brokenString[2])
 			verse.Chapter, verse.Verse = ParseChapterVerse(brokenString[3])
-			continue
-		}
-		if IsNumberedBook(brokenString[0]) {
+			verse.Text = strings.Join(brokenString[4:], " ")
+		} else if IsNumberedBook(brokenString[0]) {
 			verse.Book = brokenString[0] + brokenString[1]
-			fmt.Println(verse)
 			verse.Chapter, verse.Verse = ParseChapterVerse(brokenString[2])
-			if verse.Book == "1Timothy" {
-				fmt.Println(verse)
-				os.Exit(0)
-			}
+			verse.Text = strings.Join(brokenString[3:], " ")
 		} else {
 			verse.Book = brokenString[0]
 			verse.Chapter, verse.Verse = ParseChapterVerse(brokenString[1])
+			verse.Text = strings.Join(brokenString[2:], " ")
 		}
 
 		fmt.Printf("verse: %v\n", verse)
+		sqlInsertStr := `INSERT OR REPLACE INTO kjv(book, chapter, verse, text) values(?, ?, ?, ?)`
+		stmt, err := database.Prepare(sqlInsertStr)
+		if err != nil {
+			panic(err)
+		}
+
+		defer stmt.Close()
+		stmt.Exec(verse.Book, verse.Chapter, verse.Verse, verse.Text)
 
 	}
 
