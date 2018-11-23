3052d041c6bd8627bdac95ace999859fc1ab1a66
r4wm <raymondmintz11@gmail.com>
2018-11-23 14:07:27 -0800

    Optimize (#2)
    * prep
    
    * from minutes to seconds, yay

--- create_db.go
+++ create_db.go
@@ -4,34 +4,41 @@ import (
 	"bufio"
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Verse the complete verse context
 type Verse struct {
-	Book    string
-	Chapter int
-	Verse   int
-	Text    string
+	IsNumberedBook bool
+	Book           string
+	Chapter        int
+	Verse          int
+	Text           string
 }
 
 //ParseChapterVerse extract chapter and verse from x:x format
 func ParseChapterVerse(colonJoined string) (int, int) {
 	fmt.Printf("colonJoined: %v\n", colonJoined)
 
-	result := strings.Split(colonJoined, ":")
-	ch, err := strconv.Atoi(result[0])
+	splitChapterVerse := strings.Split(colonJoined, ":")
+
+	chapter, err := strconv.Atoi(splitChapterVerse[0])
 	if err != nil {
 		panic(err)
 	}
-	v, err := strconv.Atoi(result[1])
+
+	verseNum, err := strconv.Atoi(splitChapterVerse[1])
 	if err != nil {
 		panic(err)
 	}
-	return ch, v
+
+	return chapter, verseNum
 
 }
 
@@ -44,15 +51,43 @@ func IsNumberedBook(firstPart string) bool {
 	return false
 }
 
-func pullKJVText() {
-	fmt.Println("Starting sqlite3 db creation. ")
-	database, _ := sql.Open("sqlite3", "data/kjv.sqlite3.db")
+//PrepareDB Inserts verse context into database. Note old db WILL be deleted.
+func PrepareDB(verse <-chan Verse, dbPath string) {
+
+	// Delete old existing database
+	if _, err := os.Stat(dbPath); !os.IsNotExist(err) {
+		if err := os.Remove(dbPath); err != nil {
+			log.Fatalf("Could not remove old database: %s", dbPath)
+		}
+	}
+
+	//Create new database
+	database, _ := sql.Open("sqlite3", dbPath) // "data/kjv.sqlite3.db")
 	defer database.Close()
 
+	//Prep new database
 	statement, _ := database.Prepare("create table if not exists kjv(book string not null, chapter int, verse int, text string)")
 	statement.Exec()
 
+	sqlInsertStr := `INSERT OR REPLACE INTO kjv(book, chapter, verse, text) values(?, ?, ?, ?)`
+	stmt, err := database.Prepare(sqlInsertStr)
+	if err != nil {
+		panic(err)
+	}
+
+	//Populate, put into database as they come
+	defer stmt.Close()
+	for v := range verse {
+		stmt.Exec(v.Book, v.Chapter, v.Verse, v.Text)
+	}
+}
+
+//CreateKJVDB pulls down KJV raw text file, parses and creates database
+func CreateKJVDB() {
+	fmt.Println("Starting sqlite3 db creation. ")
+
 	url := "https://raw.githubusercontent.com/R4wm/bible/master/data/bible.txt"
+	dbInsert := make(chan Verse)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -61,7 +96,10 @@ func pullKJVText() {
 
 	defer resp.Body.Close()
 
+	go PrepareDB(dbInsert, "/tmp/kjv.db")
+
 	scanner := bufio.NewScanner(resp.Body)
+
 	for scanner.Scan() {
 		verse := Verse{}
 		brokenString := strings.Fields(scanner.Text())
@@ -83,30 +121,13 @@ func pullKJVText() {
 		}
 
 		fmt.Printf("verse: %v\n", verse)
-		sqlInsertStr := `INSERT OR REPLACE INTO kjv(book, chapter, verse, text) values(?, ?, ?, ?)`
-		stmt, err := database.Prepare(sqlInsertStr)
-		if err != nil {
-			panic(err)
-		}
 
-		defer stmt.Close()
-		stmt.Exec(verse.Book, verse.Chapter, verse.Verse, verse.Text)
-
+		dbInsert <- verse
 	}
 
+	close(dbInsert)
 }
 
 func main() {
-
-	pullKJVText()
-
-	// fmt.Println("Starting sqlite3 db creation. ")
-	// database, _ := sql.Open("sqlite3", "data/kjv.sqlite3.db")
-	// defer database.Close()
-
-	// statement, _ := database.Prepare("create table if not exists kjv(book string not null, chapter int, verse int, text string)")
-	// statement.Exec()
-
-	// fmt.Println("Database done.")
-
+	CreateKJVDB()
 }
